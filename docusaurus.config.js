a2bd7c0599571e463b403eb168d2b01b1637488a
Greg McMillan <gmcmillan100@gmail.com>
2025-02-21 21:14:47 -0800

    push 7

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://gmcmillan100.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/kb/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
